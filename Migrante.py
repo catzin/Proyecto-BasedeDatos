eb8209daa43d8e8a26ab86391dea3b6cbfbcb8ea
catzin <ehecatzin96@hotmail.com>
2019-06-07 23:48:13 -0500

    Agrego la clase Migrante , que facilita las inserciones a la base

---
+++ Migrante.py
@@ -0,0 +1,13 @@
+class Migrante:
+    def __init__(self,nombre,Ecivil,fechaNacimiento,sexo,PaisNa,religion,idMigrante,idGradoMaximo):
+        
+        self.nombre = nombre
+        self.Ecivil = Ecivil
+        self.fechaNacimiento = fechaNacimiento
+        self.sexo = sexo
+        self.PaisNa = PaisNa
+        self.religion = religion
+        self.idMigrante = idMigrante
+        self.idGradoMaximo = idGradoMaximo
+    
+    
